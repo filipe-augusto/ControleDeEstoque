0b86753218ffcb9fd738a5f472f9c5d19f41b9ab
filipe augusto <67299107+filipe-augusto@users.noreply.github.com>
2022-04-23 23:07:18 -0300

    Add project files.

---
+++ Controllers/ContaController.cs
@@ -0,0 +1,68 @@
+﻿using ControleDeEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ControleDeEstoque.Controllers
+{
+    public class ContaController : Controller
+    {
+      
+        [AllowAnonymous]
+        public ActionResult Login(string returnURl)
+        {
+            ViewBag.ReturnUrl = returnURl;
+
+            return View();
+
+        }
+
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(LoginViewModel login, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            var achou = (login.Usuario == "filipeaugusto" && login.Senha == "123");
+            if (achou)
+            {
+                //GERAR O COOKIE DE AUTENTICAÇÃO
+
+                FormsAuthentication.SetAuthCookie(login.Usuario, login.LembrarMe);
+                //DEPOIS REDIRECIONAR PARA URL QUE ELE IA ACESSAR
+                //ANTES VALIDAR SE A URL ESTA DENTRO DO DOMINIO
+                if (Url.IsLocalUrl(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                   RedirectToAction("Index", "Home");
+                }
+            }//se o login estiver erraddo
+            else
+            {
+                ModelState.AddModelError("", "login invalido!");
+            }
+
+
+            return View(login);
+        }
+
+        [AllowAnonymous]
+    [HttpPost]
+    public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ControleDeEstoque.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Informe o usuario")]
+        [Display(Name ="Usuário: ")]
+        public string Usuario { get; set; }
+        [Required(ErrorMessage = "Informe a senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha: ")]
+        public string Senha { get; set; }
+        [Display(Name = "Lembrar me: ")]
+        public bool LembrarMe { get; set; }
+
+    }
+}
