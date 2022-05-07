0df5c413245b7a850101318deb85f2237f915d99
filipe augusto <67299107+filipe-augusto@users.noreply.github.com>
2022-05-07 17:23:00 -0300

    aula 10

--- Controllers/CadastroController.cs
+++ Controllers/CadastroController.cs
@@ -10,14 +10,85 @@ namespace ControleDeEstoque.Controllers
     public class CadastroController : Controller
     {
 
-        private static List<GrupoProdutoModel> _listaGrupoProduto
-            = new List<GrupoProdutoModel>()
+        private static List<GrupoProdutoModel> _listaGrupoProduto = new List<GrupoProdutoModel>()
+        {
+            new GrupoProdutoModel() { Id=1, Nome="Livros", Ativo=true },
+            new GrupoProdutoModel() { Id=2, Nome="Mouses", Ativo=true },
+            new GrupoProdutoModel() { Id=3, Nome="Monitores", Ativo=false }
+        };
+
+        [Authorize]
+        public ActionResult GrupoProduto()
+        {
+            return View(_listaGrupoProduto);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult RecuperarGrupoProduto(int id)
+        {
+            return Json(_listaGrupoProduto.Find(x => x.Id == id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult ExcluirGrupoProduto(int id)
+        {
+            var ret = false;
+
+            var registroBD = _listaGrupoProduto.Find(x => x.Id == id);
+            if (registroBD != null)
             {
-                         new GrupoProdutoModel() { Id = 1, Nome ="LIVROS",Ativo=true},
-            new GrupoProdutoModel() { Id = 2, Nome = "MOUSE", Ativo = true },
-            new GrupoProdutoModel() { Id = 3, Nome = "COMPUTADORES", Ativo = true },
-            new GrupoProdutoModel() { Id = 4, Nome = "CELULAR", Ativo = true },
-            };
+                _listaGrupoProduto.Remove(registroBD);
+                ret = true;
+            }
+
+            return Json(ret);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
+        {
+            var resultado = "OK";
+            var mensagens = new List<string>();
+            var idSalvo = string.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "AVISO";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    var registroBD = _listaGrupoProduto.Find(x => x.Id == model.Id);
+
+                    if (registroBD == null)
+                    {
+                        registroBD = model;
+                        registroBD.Id = _listaGrupoProduto.Max(x => x.Id) + 1;
+                        _listaGrupoProduto.Add(registroBD);
+                    }
+                    else
+                    {
+                        registroBD.Nome = model.Nome;
+                        registroBD.Ativo = model.Ativo;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    resultado = "ERRO";
+                }
+            }
+
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        }
+
+
+
+
 
         [Authorize]
         public ActionResult Index()
@@ -26,11 +97,7 @@ namespace ControleDeEstoque.Controllers
 
             return View();
         }
-        [Authorize]
-        public ActionResult GrupoProduto()
-        {
-            return View(_listaGrupoProduto);
-        }
+
         [Authorize]
         public ActionResult MarcaProduto()
         {
